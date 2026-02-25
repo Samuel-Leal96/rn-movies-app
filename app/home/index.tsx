d732765bf741a396e491be0e56738361cabd1328
Samuel-Leal96 <saam_lv96@hotmail.com>
2026-02-25 17:55:08 -0600

    Carousel de imagenes

--- app/home/index.tsx
+++ app/home/index.tsx
@@ -1,17 +1,35 @@
+import { ActivityIndicator, Text, View } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+
+import MainSlideShow from '@/presentation/components/MainSlideShow';
 import { useMovies } from '@/presentation/hooks/useMovies';
-import React from 'react';
-import { Text, View } from 'react-native';
 
 const HomeScreen = () => {
 
+    const safeArea = useSafeAreaInsets();
+
     const { nowPlayingQuery } = useMovies();
 
+    if (nowPlayingQuery.isLoading) {
+
+        return (
+            <View className='justify-center items-center flex-1'>
+                <ActivityIndicator color={"blue"} size={45} />
+            </View>
+        );
+    }
+
+
     return (
-        <View>
-            <Text>HomeScreen</Text>
-            <Text>{JSON.stringify(nowPlayingQuery.data)}</Text>
+        <View className='mt-2' style={{ marginTop: safeArea.top }}>
+            <Text className='text-3xl font-bold px-4 mb-2' >Home Screen</Text>
+
+            {/* Carousel de imágenes */}
+
+            <MainSlideShow movies={nowPlayingQuery.data ?? []} />
         </View>
-    )
+    );
+
 }
 
 export default HomeScreen
